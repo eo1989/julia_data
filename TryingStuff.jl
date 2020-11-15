f604c08882e3bfb6edda2e26e0c15a8954621d06
eo1989 <eorlowski6@gmail.com>
2020-11-15 13:19:43 -0500

    finished the example

--- TryingStuff.jl
+++ TryingStuff.jl
@@ -12,3 +12,15 @@ age  vol
 90   420
 145  502
 """), delim=" ", ignorerepeated=true, copycols=true)
+
+par0 = [600, 0.02, 60]
+
+fit = curve_fit(model, obsVols.age, obsVols.vol, par0)
+
+fit.param = [497.07, 0.05, 53.5]
+fitX = 0:maximum(obsVols.age)*1.2
+fitY = [fit.param[1] / (1 + exp(-fit.param[2] * (x - fit.param[3]))) for x in fitX]
+
+plot(fitX, fitY, seriestype=:line, label = "Fitted values")
+plot!(obsVols.age, obsVols.vol, seriestype = :scatter, label = "Obs values")
+plot!(obsVols.age, fit.resid, seriestype=:bar, label = "Residuals")
